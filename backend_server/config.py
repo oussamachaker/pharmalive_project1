3fe0715c3e3489b45d02a796ec95356bddff79c2
s.dassi <s.dassi@instadeep.com>
2020-04-16 23:24:47 +0100

    authentication models and views

---
+++ backend_server/config.py
@@ -0,0 +1,19 @@
+import os
+
+postgres_local_base = 'postgresql://postgres:@localhost/'
+database_name = 'flask_jwt_auth'
+
+
+
+class BaseConfig:
+    """Base configuration."""
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious') #SECRET_KEY has to be generated with os.urandom(24) then exported : environment variable 
+    DEBUG = False
+    BCRYPT_LOG_ROUNDS = 13
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration."""
+    DEBUG = True
+    BCRYPT_LOG_ROUNDS = 4
+    SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name

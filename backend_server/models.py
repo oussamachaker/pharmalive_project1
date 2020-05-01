7306bcc03c4429bf7156fdfb7e31059a91dbee77
s.dassi <s.dassi@instadeep.com>
2020-05-01 22:43:27 +0100

    added yosra's models

--- backend_server/models.py
+++ backend_server/models.py
@@ -1,6 +1,8 @@
 import datetime
 import jwt
+import uuid
 
+from sqlalchemy.orm import relationship, backref
 from backend_server import app, db, bcrypt
 
 
@@ -9,18 +11,26 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(255), index=False, unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
     admin = db.Column(db.Boolean, nullable=False, default=False)
 
-    def __init__(self, email, password, admin=False):
+    adress = db.relationship('pharmacies', backref=backref('users',uselist = False))
+    product = db.relationship('products', backref=backref('users',uselist = False))
+
+    def __init__(self, username, email, password, admin=False):
         self.email = email
+        self.username = username
         self.password = bcrypt.generate_password_hash(
             password, app.config.get('BCRYPT_LOG_ROUNDS')
         ).decode()
         self.registered_on = datetime.datetime.now()
         self.admin = admin
+    
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
     def encode_auth_token(self, user_id):
         """ Generates the Auth Token: return string """
@@ -83,3 +93,82 @@ class BlacklistToken(db.Model):
             return True  
         else:
             return False
+
+
+class pharmacies(db.Model):
+    """Model for pharmaLive pharmacies ."""
+
+    __tablename__ = 'pharmacies'
+    id = db.Column(db.Integer,
+                   primary_key=True)
+    pharmacy_name = db.Column(db.String(64),
+                         index=False,
+                         unique=False,
+                         nullable=True)
+    address = db.Column(db.String(64),
+                         index=True,
+                         unique=True,
+                         nullable=False)
+    phone_number = db.Column(db.Integer,
+                      index=False,
+                      unique=True,
+                      nullable=True)
+    longitude = db.Column(db.Integer,
+                      index=False,
+                      unique=True,
+                      nullable=False)
+    latitude = db.Column(db.Integer,
+                        index=False,
+                        unique=True,
+                        nullable=False)
+    pharmacist_id = db.Column(db.String,
+                    db.ForeignKey('Users.username'),
+                    nullable=False)
+    pharmacist = relationship("users", backref="pharmacies")
+
+    def __init__(self,pharmacy_name, address ,phone_number, longitude, latitude,pharmacist_id,available):
+        self.id = uuid.uuid4().hex
+        self.pharmacy_name = pharmacy_name
+        self.address = address
+        self.phone_number = phone_number
+        self.longitude = longitude
+        self.latitude = latitude
+        self.pharmacist_id = pharmacist_id
+        self.available = available
+
+    def __repr__(self):
+        return '<pharmacies {}\n{}\n{}>'.format(self.pharmacist_id, self.address, self.available)
+
+
+class products(db.Model):
+    """Model for pharmaLive products ."""
+
+    __tablename__ = 'products'
+    id = db.Column(db.Integer,
+                   primary_key=True)
+    product_name = db.Column(db.String(64),
+                         index=False,
+                         unique=False,
+                         nullable=False)
+    quantity = db.Column(db.Integer,
+                      index=False,
+                      unique=False,
+                      nullable=True)
+    last_update = db.Column(db.DateTime,
+                        index=True,
+                        unique=False,
+                        nullable=False)
+    pharmacist_id = db.Column(db.String,
+                    db.ForeignKey('users.username'),
+                    nullable=False)
+    pharmacist = relationship("users", backref="products")                    
+
+    def __init__(self,pharmacist_id , product_name, quantity,last_update):
+        self.id = uuid.uuid4().hex
+        self.pharmacist_id = pharmacist_id
+        self.product_name = product_name
+        self.quantity = quantity
+        self.last_update = last_update
+
+    def __repr__(self):
+        return '<produits {}\n{}\n{}>'.format(self.pharmacist_id, self.produit_name, self.stock)

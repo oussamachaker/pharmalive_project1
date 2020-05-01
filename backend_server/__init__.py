27aab738ed7358cf6e4e397d2d02f46ca0546dba
GorsanMestiri <k.gorsan@instadeep.com>
2020-05-01 23:51:48 +0100

    Adding marshmallow instance to the app configs

--- backend_server/__init__.py
+++ backend_server/__init__.py
@@ -3,19 +3,21 @@ import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-app_settings = os.getenv(
-    'APP_SETTINGS',
-    'backend_server.config.DevelopmentConfig'
-)
+app_settings = os.getenv("APP_SETTINGS", "backend_server.config.DevelopmentConfig")
 app.config.from_object(app_settings)
 
 bcrypt = Bcrypt(app)
+# Init db
 db = SQLAlchemy(app)
+# Init marshmallow
+ma = Marshmallow(app)
 
 from backend_server.auth.views import auth_blueprint
+
 app.register_blueprint(auth_blueprint)
